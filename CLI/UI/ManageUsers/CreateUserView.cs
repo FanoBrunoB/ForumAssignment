332b1ab4681b9b05f793b429ca12bb39ddb023a6
Negru Cornel <kornel.negru@gmail.com>
2026-09-21 21:58:24 +0200

    Update CreateUserView.cs

--- CLI/UI/ManageUsers/CreateUserView.cs
+++ CLI/UI/ManageUsers/CreateUserView.cs
@@ -9,14 +9,23 @@ public class CreateUserView(IUserRepository userRepository)
     {
         Console.Write("Username: ");
         string? username = Console.ReadLine();
+        bool usernameExists = userRepository.GetMany().Any(u => u.Username == username);
+
+        if (usernameExists)
+        {
+            Console.WriteLine("That username is already taken.");
+            return;
+        }
         Console.Write("Password: ");
         string? password = Console.ReadLine();
-
+        
+        
         User user = new User(username, password)
         {
             Username = username,
             Password = password
         };
+        
         
         await userRepository.AddAsync(user);
         
